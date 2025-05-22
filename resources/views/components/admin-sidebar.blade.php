c91169bffdfd4b9faa13404b9b51099a5e6672da
Nawodi Thathsarani <n.t.priyawansha@gmail.com>
2025-05-22 15:42:03 +0530

    changed workout builder to manager

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -61,7 +61,7 @@
                     <!-- Icon -->
                     <i class="bi bi-list-check w-5 h-5 m-3" onclick="toggleSidebar()"></i>
                     <!-- Text -->
-                    <span class="sidebar-item-text whitespace-nowrap">Workout Builder</span>
+                    <span class="sidebar-item-text whitespace-nowrap">Workout Manager</span>
                 </a>
                 <hr class="border-t border-black" />
             </li>
