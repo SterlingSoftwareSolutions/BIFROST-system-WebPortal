84b4c89af43d647d44212824de07d4fd317113cd
Thafani Nawaz <114370326+Thafani24@users.noreply.github.com>
2025-06-30 15:17:00 +0530

    removed workout timer tab

--- resources/views/mobile/components/mobile-footer.blade.php
+++ resources/views/mobile/components/mobile-footer.blade.php
@@ -7,7 +7,7 @@
         <div class="dot w-4 h-4 bg-transparent border border-white rounded-full transition duration-300 ease-in-out cursor-pointer"></div>
         <div class="dot w-4 h-4 bg-transparent border border-white rounded-full transition duration-300 ease-in-out cursor-pointer"></div>
         <div class="dot w-4 h-4 bg-transparent border border-white rounded-full transition duration-300 ease-in-out cursor-pointer"></div>
-        <div class="dot w-4 h-4 bg-transparent border border-white rounded-full transition duration-300 ease-in-out cursor-pointer"></div>
+        {{-- <div class="dot w-4 h-4 bg-transparent border border-white rounded-full transition duration-300 ease-in-out cursor-pointer"></div> --}}
         <div class="dot w-4 h-4 bg-transparent border border-white rounded-full transition duration-300 ease-in-out cursor-pointer"></div>
         <div id="right-arrow" class="arrow w-16 md:w-20 cursor-pointer">
             <img src="{{ asset('images/next.png') }}" alt="Next" class="w-4 h-4" />
@@ -26,7 +26,7 @@
             '{{ route('mobile.trainingday') }}',
             '{{ route('mobile.readinessscore') }}',
             '{{ route('mobile.workout') }}',
-            '{{ route('mobile.workouttimer') }}',
+            // '{{ route('mobile.workouttimer') }}',
             '{{ route('mobile.histroyview') }}',
            
         ];
